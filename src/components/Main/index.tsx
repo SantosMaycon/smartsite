54d52aafe9ab89a0e6e4f80aca5d756f645b1ac9
SantosMaycon <mayconalves.santos14@gmail.com>
2021-07-26 22:31:37 -0300

    start project

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>Boilerplate - NextJS</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS and Styled Components
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
